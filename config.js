bc6d40618038ce1647f426ca6266a223cce39b53
Suraj Bobade <iamsurajbobade@gmail.com>
2021-07-24 12:30:08 +0530

    config update

--- config.js
+++ config.js
@@ -11,23 +11,23 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'Suraj',
+  name: "Suraj",
   imageBackground: false,
   openInNewTab: true,
 
   // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+  greetingMorning: "Good morning!",
+  greetingAfternoon: "Good afternoon,",
+  greetingEvening: "Good evening,",
+  greetingNight: "Go to Sleep!",
 
   // Weather
-  weatherKey: '${{ secrets.OPEN_WEATHER_MAP_KEY }}',
-  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'C',
-  weatherLatitude: '18.520430',
-  weatherLongitude: '73.856743',
-  
+  weatherKey: "${{ secrets.OPEN_WEATHER_MAP_KEY }}",
+  weatherIcons: "OneDark", // 'Nord', 'Dark', 'White'
+  weatherUnit: "C",
+  weatherLatitude: "18.520430",
+  weatherLongitude: "73.856743",
+
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
   // └─┘┴ ┴┴└──┴┘└─┘
@@ -35,40 +35,40 @@ const CONFIG = {
   // Links
   cards: [
     {
-      id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/IAmSurajBobade',
+      id: "1",
+      name: "Github",
+      icon: "github",
+      link: "https://github.com/IAmSurajBobade",
     },
     {
-      id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'mailto:iamsurajbobade@gmail.com',
+      id: "2",
+      name: "Mail",
+      icon: "mail",
+      link: "https://mail.google.com/mail/u/0/#inbox",
     },
     {
-      id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://calendar.google.com/calendar/r',
+      id: "3",
+      name: "Todoist",
+      icon: "trello",
+      link: "https://calendar.google.com/calendar/r",
     },
     {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      id: "4",
+      name: "Calendar",
+      icon: "calendar",
+      link: "https://calendar.google.com/calendar/r",
     },
     {
-      id: '5',
-      name: 'Reddit',
-      icon: 'bookmark',
-      link: 'https://reddit.com',
+      id: "5",
+      name: "Reddit",
+      icon: "bookmark",
+      link: "https://reddit.com",
     },
     {
-      id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
+      id: "6",
+      name: "Odysee",
+      icon: "youtube",
+      link: "https://odysee.com/",
     },
   ],
 
@@ -77,45 +77,45 @@ const CONFIG = {
   // ┴─┘┴└─┘ ┴ └─┘
 
   //Icons
-  firstListIcon: 'music',
-  secondListIcon: 'coffee',
+  firstListIcon: "music",
+  secondListIcon: "coffee",
 
   // Links
   lists: {
     firstList: [
       {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Inspirational",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
       {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Classic",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
       {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Oldies",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
       {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Rock",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
     ],
     secondList: [
       {
-        name: 'Linkedin',
-        link: 'https://linkedin.com/',
+        name: "Linkedin",
+        link: "https://linkedin.com/",
       },
       {
-        name: 'Figma',
-        link: 'https://figma.com/',
+        name: "Figma",
+        link: "https://figma.com/",
       },
       {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
+        name: "Dribbble",
+        link: "https://dribbble.com",
       },
       {
-        name: 'Telegram',
-        link: 'https://webk.telegram.org',
+        name: "Telegram",
+        link: "https://webk.telegram.org",
       },
     ],
   },
